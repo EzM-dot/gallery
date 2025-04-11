2edcc810bc6c6de2929776900887429722ddf8dc
EzM-dot <musyokaedwin@gmail.com>
2025-04-11 03:26:03 +0300

    Added json files

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,26 +1,29 @@
-process.env.NODE_ENV = 'test';   
+process.env.NODE_ENV = 'test';
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
+const chai = require('chai');
+const server = require('../server');
+const should = chai.should();
+const expect = chai.expect;
 
-var server = require('../server');
-var should = chai.should();
-var expect = chai.expect;
+(async () => {
+    const chaiHttp = (await import('chai-http')).default; // Dynamically import chai-http
+    chai.use(chaiHttp); // Use chai-http after it is imported
 
-chai.use(chaiHttp);
-
-describe('Photos', function(){
-
-
-    it('should list ALL photos on / GET', function(done){
-        this.timeout(60000);
-        chai.request(server)
-        .get('/')
-        .end(function(err,res){
-            res.should.have.status(200);
-            res.should.be.html;
-            res.body.should.be.a('object')
-            done();
-        })
+    // Define tests inside the async block
+    describe('Photos', function () {
+        it('should list ALL photos on / GET', function (done) {
+            this.timeout(60000);
+            chai.request(server)
+                .get('/')
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    res.should.be.html;
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
     });
-})
+
+    // Run the tests
+    run(); // Mocha's `run()` function to execute tests dynamically
+})();
